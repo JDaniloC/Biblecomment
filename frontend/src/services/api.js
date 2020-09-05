b68ba2eba13831db3995b3df65689b34941bd1ff
JDaniloC <jdsc@cin.ufpe.br>
2020-09-05 20:37:54 -0300

    heroku deploy front

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "https://biblecomment-backend.herokuapp.com:7603"
 })
 
 export default api;
