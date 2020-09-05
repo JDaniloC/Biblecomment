2153fc87575e3ad023bb5d9f29c20538a0bc3749
JDaniloC <jdsc@cin.ufpe.br>
2020-09-05 20:15:14 -0300

    heroku frontend deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,4 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+const port = process.env.PORT || 3333
+app.listen(
+  port, () => console.log("Server started on port", port));
