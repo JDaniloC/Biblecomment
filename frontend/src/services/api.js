7db23dba1e8475e988d7e9aff0918785db777a99
JDaniloC <jdsc@cin.ufpe.br>
2020-09-05 23:18:07 -0300

    fix url api backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://biblecomment-backend.herokuapp.com:7603"
+    baseURL: "https://biblecomment-backend.herokuapp.com"
 })
 
 export default api;
