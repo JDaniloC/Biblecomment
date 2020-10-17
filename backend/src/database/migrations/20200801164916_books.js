0fe3c1708b680f351f707a500e8281ef5bfe8258
Danilo Silva <jdsc@cin.ufpe.br>
2020-10-17 20:06:44 -0300

    timestamp fixed

--- backend/src/database/migrations/20200801164916_books.js
+++ backend/src/database/migrations/20200801164916_books.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.dropTableIfExists('books')
     .createTable('books', function(table) {    
-        table.timestamp("created_at");
+        table.timestamp("created_at").defaultTo(knex.fn.now());
         table.string('abbrev').primary();
 
         table.string('title').notNullable();
