ab9965a82db38540c16884be687b24736b5864c7
Danilo Silva <jdsc@cin.ufpe.br>
2020-10-17 18:54:59 -0300

    new testment

--- scrapy/webscraping.py
+++ scrapy/webscraping.py
@@ -1,5 +1,5 @@
 import re, json, time, requests, os
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -37,63 +37,69 @@ def versesTotal(abbrev, chapter, token):
     result = response.json()
     return result["chapter"]["verses"]
 
-abbrev = input("Abbreviation: ")
-browser = Chrome()
-error = False
+book_list = []
 
-chapter = 1
-totalChapters = requests.get(
-        f"https://bibleapi.co/api/books/{abbrev}/", 
-        headers = {"Authorization": "Bearer " + token},
-    ).json()["chapters"]
-book = {}
+opcoes = ChromeOptions()
+opcoes.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+browser = Chrome(chrome_options = opcoes)
 
-while chapter <= totalChapters and not error:
-    print(f"Capturing the chapter {chapter}")
-    # It enter in the chapter and wait to load
+for abbrev in book_list:
+    error = False
 
-    oldId = browser.find_element_by_tag_name("html").id
-    browser.get(f"https://pesquisa.biblia.com.br/pt-BR/RA/{abbrev}/{str(chapter)}")
-    error = not waitToLoad(browser, oldId)
-    
-    if error:
-        print("Script interruted")
-        continue
+    chapter = 1
+    totalChapters = requests.get(
+            f"https://bibleapi.co/api/books/{abbrev}/", 
+            headers = {"Authorization": "Bearer " + token},
+        ).json()["chapters"]
+    book = {}
 
-    # It scroll to the end for load full page
-    browser.execute_script("window.scrollBy(0, document.body.scrollHeight)")
-    time.sleep(0.6)
+    while chapter <= totalChapters and not error:
+        print(f"Capturing the chapter {chapter}")
+        # It enter in the chapter and wait to load
 
-    # It capture the verses
-    total = versesTotal(abbrev, chapter, token)
-    verses = browser.find_elements_by_css_selector("li[class=versiculoTexto]")
-    cont = 0
-    while len(verses) != total and cont < 20:
+        oldId = browser.find_element_by_tag_name("html").id
+        browser.get(f"https://pesquisa.biblia.com.br/pt-BR/RA/{abbrev}/{str(chapter)}")
+        error = not waitToLoad(browser, oldId)
+        
+        if error:
+            print("Script interruted")
+            continue
+
+        # It scroll to the end for load full page
         browser.execute_script("window.scrollBy(0, document.body.scrollHeight)")
         time.sleep(0.6)
+
+        # It capture the verses
+        total = versesTotal(abbrev, chapter, token)
         verses = browser.find_elements_by_css_selector("li[class=versiculoTexto]")
-        cont += 1
+        cont = 0
+        while len(verses) != total and cont < 20:
+            browser.execute_script("window.scrollBy(0, document.body.scrollHeight)")
+            time.sleep(0.6)
+            verses = browser.find_elements_by_css_selector("li[class=versiculoTexto]")
+            cont += 1
 
-    # It create a new dictionary of the chapter
-    book[chapter] = []
-    for verse in verses:
-        # It split the verse number of the verse text
-        verse_number, verse_text = [x for x in re.split(r'(\d+)\s+([\w\W]*)', verse.text) if x != ""]
-        book[chapter].append(verse_text.strip())
+        # It create a new dictionary of the chapter
+        book[chapter] = []
+        for verse in verses:
+            # It split the verse number of the verse text
+            verse_number, verse_text = [x for x in re.split(r'(\d+)\s+([\w\W]*)', verse.text) if x != ""]
+            book[chapter].append(verse_text.strip())
 
-    chapter += 1
+        chapter += 1
+
+    if not error:
+        print("Creating File")
+        with open(os.path.join("chapters", f"{abbrev}.json"), "w", 
+            encoding="utf-8") as file:
+            json.dump(book, file, ensure_ascii=False, indent = 2)
+
+        # Add the chapters number in the books.json
+        with open("books.json", encoding="utf-8") as file:
+            books = json.load(file)
+        with open("books.json", "w", encoding="utf-8") as file:
+            books.update({abbrev: totalChapters})
+            json.dump(books, file, indent = 2)
+        print("Files created!")
 
 browser.quit()
-
-if not error:
-    print("Creating File")
-    with open(os.path.join("chapters", f"{abbrev}.json"), "w", encoding="utf-8") as file:
-        json.dump(book, file, ensure_ascii=False, indent = 2)
-
-    # Add the chapters number in the books.json
-    with open("books.json") as file:
-        books = json.load(file)
-    with open("books.json", "w") as file:
-        books.update({abbrev: totalChapters})
-        json.dump(books, file, indent = 2)
-    print("Files created!")
