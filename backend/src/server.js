5dd3977ff78264cb0b17872a32d83bd322d5b881
Danilo Silva <jdsc@cin.ufpe.br>
2020-10-24 13:01:45 +0000

    allow cors on all routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 const routes = require("./routes");
 const app = express();
 
-app.use(cors({
+app.options('*', cors({
   origin: 'http://www.biblecomment.net',
   optionsSuccessStatus: 200 
-}));
+}))
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
