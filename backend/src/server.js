5b5df93255c72a3afdf5f962923d569e8d669742
Danilo Silva <jdsc@cin.ufpe.br>
2020-10-24 12:44:43 +0000

    cors origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,12 @@ const cors = require('cors');
 const routes = require("./routes");
 const app = express();
 
-app.use(cors());
+app.use((req, res) => {
+    res.header("Access-Control-Allow-Origin", "http://www.biblecomment.net");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+});
+
 app.use(express.json());
 app.use(routes);
 
