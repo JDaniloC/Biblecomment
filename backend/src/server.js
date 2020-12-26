6009f3fa570290b8ef3e835a8ee9b40e170feea9
Danilo Silva <49529180+JDaniloC@users.noreply.github.com>
2020-12-26 08:27:20 -0300

    .com.br added

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,9 +9,13 @@ app.options('*', cors({
     'http://biblecomment.net', 
     'http://www.biblecomment.net',
     'http://biblecomment.netlify.app',
+    'http://www.biblecomment.com.br',
+    'http://biblecomment.com.br',
     'https://biblecomment.net', 
     'https://biblecomment.netlify.app',
-    'https://www.biblecomment.net'],
+    'https://www.biblecomment.net',
+    'https://biblecomment.com.br',
+    'https://www.biblecomment.com.br'],
   optionsSuccessStatus: 200 
 }))
 app.use(cors());
