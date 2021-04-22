d932a18aaca3989437c16b3139de1ea32b095475
Danilo Silva <jdsc@cin.ufpe.br>
2021-04-22 10:31:36 -0300

    mend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://backend.biblecomment.net/"
+    baseURL: "https://biblecomment.herokuapp.com/"
 })
 
 export default api;
