d58b37a349a10bd3a33d051f793447778877c5e8
Danilo Silva <jdsc@cin.ufpe.br>
2021-06-24 14:04:04 -0300

    box-shadow

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById("root")
 );
 
