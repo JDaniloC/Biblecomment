ee9ea97e724728c481bda92c61d2c8b9fd3d4568
Danilo Silva <jdsc@cin.ufpe.br>
2021-09-19 22:54:15 -0300

    fix index

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -5,26 +5,20 @@ import { HelpButton } from "../../components/Partials";
 import BooksIndex from "../../components/BooksIndex";
 import logoImg from "../../assets/logo.png";
 import Login from "../../components/Login";
-import React, { Component } from "react";
+import React from "react";
 
-export default class Main extends Component {
-	shouldComponentUpdate() {
-		return false;
-	}
-
-	render() {
-		return (
-			<div className="panel">
-				<HelpButton />
-				<div className="logo-container">
-					<img src={logoImg} alt="logo" />
-					<p>
-						Compartilhe a mensagem de Deus <br /> com seus irmãos
-					</p>
-					<Login ref={this.loginComponent} />
-				</div>
-				<BooksIndex />
+export default function Main() {
+	return (
+		<div className="panel">
+			<HelpButton />
+			<div className="logo-container">
+				<img src={logoImg} alt="logo" />
+				<p>
+					Compartilhe a mensagem de Deus <br /> com seus irmãos
+				</p>
+				<Login/>
 			</div>
-		);
-	}
+			<BooksIndex />
+		</div>
+	);
 }
