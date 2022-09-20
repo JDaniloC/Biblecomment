ab1bf30a0c11cc94bdcfab4740380601be1fd76a
JDaniloC <jdsc@cin.ufpe.br>
2022-09-19 23:47:54 -0300

    Update all packages except react-router-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import App from "./App";
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />,);
